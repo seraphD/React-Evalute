c5e644a5495ebef79a880c68ad0cd02a4074268a
seraphD <36287146+seraphD@users.noreply.github.com>
2019-02-27 20:02:36 +0800

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports={
+	port:4000,
+	apiPort:4001,
+	host:"localhost",
+	user:'root',
+	password:'123',
+	database:'react-eval',
+	basicURL_T:'http://localhost:4000/teacher',
+	basicURL_S:'http://localhost:4000/student',
+	basicURL:'http://localhost:4000'
+};
