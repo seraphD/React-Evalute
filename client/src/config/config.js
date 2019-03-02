179dfd4a9e40218e34ccd024f4ca25ca371a5070
seraphD <36287146+seraphD@users.noreply.github.com>
2019-03-02 12:00:05 +0800

    Update config.js

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -2,10 +2,10 @@ module.exports={
 	port:4000,
 	apiPort:4001,
 	host:"localhost",
-	user:'root',
-	password:'123',
-	database:'react-eval',
-	basicURL_T:'http://localhost:4000/teacher',
-	basicURL_S:'http://localhost:4000/student',
-	basicURL:'http://localhost:4000'
+	user:'react-evaluete',
+	password:'Wingofyou12.',
+	database:'react-evaluate',
+	basicURL_T:'http://47.100.45.240:4010/teacher',
+	basicURL_S:'http://47.100.45.240:4010/student',
+	basicURL:'http://47.100.45.240:4010'
 };
