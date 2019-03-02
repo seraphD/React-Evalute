7988ac0b47f9478551578cb1393e9e9fb0f75fb8
seraphD <36287146+seraphD@users.noreply.github.com>
2019-03-02 12:32:49 +0800

    Update mysql.js

--- server/db/mysql.js
+++ server/db/mysql.js
@@ -2,7 +2,6 @@ const mysql      = require('mysql');
 const config 	 = require('../../config/config');
 
 const connection = mysql.createConnection({
-  host	   : config.host,
   user     : config.user,
   password : config.password,
   database : config.database
